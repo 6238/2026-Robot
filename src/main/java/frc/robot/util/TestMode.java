9b151f497d6f0ba3792d48414c816077198d6c5d
Ryan LeRoy <leroy.ryan09@gmail.com>
2026-01-14 19:39:28 -0800

    limited actuator test

--- src/main/java/frc/robot/util/TestMode.java
+++ src/main/java/frc/robot/util/TestMode.java
@@ -3,8 +3,8 @@ package frc.robot.util;
 import static edu.wpi.first.units.Units.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -27,11 +27,23 @@ public class TestMode {
         Time sampleTime,
         AngularVelocity targetVelocity,
         Current targetCurrent,
-        Current currentThreshold,
-        Optional<Angle> minAngle,
-        Optional<Angle> maxAngle
+        Current currentThreshold
     ) {}
 
+    public record LimitedMotorTestProfile(
+        String motorName,
+        Voltage voltage,
+        Time duration,
+        Time sampleTime,
+        AngularVelocity targetVelocity,
+        Current targetCurrent,
+        Current currentThreshold,
+        Angle minAngle,
+        Angle maxAngle,
+        Angle angleThreshold,
+        Time maxSettleTime
+    ) {}
+    
     public record EncoderTestProfile(
         String motorName,
         String encoderName,
@@ -39,41 +51,116 @@ public class TestMode {
     ) {}
 
     // A command for testing an external encoder
-    public Command testMotorEncoder(Subsystem subsystem, Consumer<TestResult> results, EncoderTestProfile testProfile,
+    public static Command testMotorEncoder(Subsystem subsystem, Consumer<TestResult> results, EncoderTestProfile testProfile,
             Consumer<Voltage> motorVoltageConsumer, Supplier<Current> motorPositionSupplier) {
         return Commands.runOnce(() -> {
         });
     }
 
-    public boolean withinThreshold(double value, double target, double threshold) {
+    public static boolean withinThreshold(double value, double target, double threshold) {
         if (value >= target - threshold && value <= target + threshold) {
             return true;
         }
         return false;
     }
 
-    public boolean belowThreshold(double value, double target, double threshold) {
+    public static boolean belowThreshold(double value, double target, double threshold) {
         if (value >= target - threshold) {
             return false;
         }
         return true;
     }
 
-    public boolean aboveThreshold(double value, double target, double threshold) {
+    public static boolean aboveThreshold(double value, double target, double threshold) {
         if (value <= target + threshold) {
             return false;
         }
         return true;
     }
 
+    public static Command testLimitedMotorOutputProfile(Subsystem subsystem, Consumer<TestResult> results, LimitedMotorTestProfile testProfile,
+            Consumer<Voltage> motorVoltageConsumer, Consumer<Angle> motorPositionConsumer, Supplier<Current> motorCurrentSupplier,
+            Supplier<AngularVelocity> motorVelocitySupplier, Supplier<Angle> motorPositionSupplier) {
+
+        TimedSampleBuffer<Current> currentSampleBuffer = new TimedSampleBuffer<Current>();
+
+        AtomicBoolean endedFlag = new AtomicBoolean(false);
+
+        return Commands.sequence(
+            Commands.runOnce(() -> motorPositionConsumer.accept(testProfile.maxAngle), subsystem), // Test Max Position
+            
+            Commands.waitTime(testProfile.maxSettleTime).andThen(
+                Commands.runOnce(() -> endedFlag.set(true))
+            ).until(() -> withinThreshold(motorPositionSupplier.get().in(Rotations), testProfile.maxAngle.in(Rotations), testProfile.angleThreshold.in(Rotations))), // Wait for motor to settle
+            
+            Commands.either(
+                
+                Commands.runOnce(() -> results.accept(new TestResult(false,
+                    "Motor " + testProfile.motorName + " failed to reach max angle: wanted: " + testProfile.maxAngle.in(Degrees) + " degrees got: " + motorPositionSupplier.get().in(Degrees) + " degrees"))),
+                
+                Commands.sequence(
+                    Commands.runOnce(() -> motorPositionConsumer.accept(testProfile.minAngle), subsystem), // Test Min Position
+                    
+                    Commands.waitTime(testProfile.maxSettleTime).andThen(
+                        Commands.runOnce(() -> endedFlag.set(true))
+                    ).until(() -> withinThreshold(motorPositionSupplier.get().in(Rotations), testProfile.minAngle.in(Rotations), testProfile.angleThreshold.in(Rotations))), // Wait for motor to settle
+                    
+                    Commands.either(
+                        Commands.runOnce(() -> results.accept(new TestResult(false,
+                            "Motor " + testProfile.motorName + " failed to reach min angle: wanted: " + testProfile.minAngle.in(Degrees) + " degrees got: " + motorPositionSupplier.get().in(Degrees) + " degrees"))),
+                        
+                        Commands.sequence(
+
+                            Commands.runOnce(() -> motorVoltageConsumer.accept(testProfile.voltage), subsystem),
+                            Commands.run(() -> {
+                                currentSampleBuffer.add(Time.ofBaseUnits(Timer.getTimestamp(), Seconds), motorCurrentSupplier.get());
+                            }).withTimeout(testProfile.duration.in(Seconds)),
+                            Commands.runOnce(() -> motorVoltageConsumer.accept(Volts.of(0)), subsystem),
+                            
+                            Commands.runOnce(() -> {
+                                // Check test results
+                                List<TimedSample<Current>> currentSamples = currentSampleBuffer.getLast(testProfile.sampleTime);
+                                OptionalDouble averageCurrent = currentSamples.stream().mapToDouble((sample) -> sample.value().in(Amps)).average();
+                                if (averageCurrent.isEmpty()) {
+                                    results.accept(new TestResult(false, "Motor " + testProfile.motorName + " current samples not available"));
+                                    return;
+                                }
+
+                                if (withinThreshold(averageCurrent.getAsDouble(), testProfile.targetCurrent.in(Amps), testProfile.currentThreshold.in(Amps))) {
+                                    results.accept(new TestResult(true,
+                                            "Motor " + testProfile.motorName + " current draw within limits: " + averageCurrent.getAsDouble()
+                                                    + "A (threshold: " + testProfile.currentThreshold.in(Amps) + "A)"));
+                                    return;
+                                }
+                                
+                                if (aboveThreshold(averageCurrent.getAsDouble(), testProfile.targetCurrent.in(Amps), testProfile.currentThreshold.in(Amps))) {
+                                    results.accept(new TestResult(false,
+                                        "Motor " + testProfile.motorName + " drew too much current: " + averageCurrent.getAsDouble()
+                                                + "A (target: " + testProfile.targetCurrent + "A)"));
+                                    return;
+                                }
+                                
+                                results.accept(new TestResult(false,
+                                    "Motor " + testProfile.motorName + " drew too little current: " + averageCurrent.getAsDouble()
+                                            + "A (target: " + testProfile.targetCurrent + "A)"));
+                            })
+                        
+                        ),
+                        () -> endedFlag.get()
+                    )
+                ),
+                () -> endedFlag.get()
+            )
+        );
+    }
+
     // A command for testing the output and freedom of movement of a motor
     // If min and max angle are supplied then the test will check that the motor can reach those angles
     // Ensure actuator software limits are enabled before running this test on a angle limited actuator
-    public Command testMotorOutputProfile(Subsystem subsystem, Consumer<TestResult> results, MotorTestProfile testProfile,
+    public static Command testMotorOutputProfile(Subsystem subsystem, Consumer<TestResult> results, MotorTestProfile testProfile,
             Consumer<Voltage> motorVoltageConsumer, Supplier<Current> motorCurrentSupplier,
             Supplier<AngularVelocity> motorVelocitySupplier) {
-        
-        
+
         TimedSampleBuffer<Current> currentSampleBuffer = new TimedSampleBuffer<Current>();
         // Test a freespinning actuator
         return Commands.sequence(
