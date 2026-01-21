3ba0fb28567d4e4a94faf2daa98eed18961ca1bc
Ryan LeRoy <leroy.ryan09@gmail.com>
2026-01-21 07:54:04 -0800

    remove object detection

--- ObjectDetectionCoprocessor/build_trt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cmake -S . -B build -G Ninja \
-  -DUSE_ONNX_RUNTIME=OFF \
-  -DUSE_TENSORRT=ON \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=/usr/bin/clang-15 \
-  -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
-
-cmake --build build --parallel  
