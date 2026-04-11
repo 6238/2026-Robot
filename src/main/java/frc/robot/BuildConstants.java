6437cc99f40ebcba3cf6cd658136e8ab09a0f863
Ryan LeRoy <leroy.ryan09@gmail.com>
2026-04-10 18:28:52 -0700

    fix stray bracket

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 95;
-  public static final String GIT_SHA = "b2574846ac9f960f8297b5cc04a85b7ecd927f20";
-  public static final String GIT_DATE = "2026-04-10 11:33:52 EDT";
+  public static final int GIT_REVISION = 96;
+  public static final String GIT_SHA = "7550f9b04b589654c0ed8de6307339ca73fa8ef9";
+  public static final String GIT_DATE = "2026-04-10 16:55:30 EDT";
   public static final String GIT_BRANCH = "recovered-work";
-  public static final String BUILD_DATE = "2026-04-10 16:54:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1775854448434L;
+  public static final String BUILD_DATE = "2026-04-10 18:15:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1775859340523L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
